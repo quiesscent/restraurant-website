0adb40318b019ea7564620afa5dc99fcc7df85cf
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-07-23 18:46:39 +0300

    created index.js and added the interactivity in top categories

---
+++ index.js
@@ -0,0 +1,17 @@
+//price
+let priceOne=document.getElementById("product-price-one");
+let priceTwo=document.getElementById("product-price-two");
+let priceThree=document.getElementById("product-price-three");
+
+
+//name
+let nameOne=document.getElementById("product-name-one");
+let nameTwo=document.getElementById("product-name-two");
+let nameThree=document.getElementById("product-name-three");
+
+//images
+let imageOne=document.getElementById("product-image-one");
+let imageTwo=document.getElementById("product-image-two");
+let imageThree=document.getElementById("product-image-three");
+
+console.log(imageThree);
