23be9b065f1db5512244e37b5f1c48844a3348ab
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-07-24 20:41:28 +0300

    added the images of bestseller images

--- index.js
+++ index.js
@@ -22,29 +22,6 @@ let featuredButtonOnclick=document.getElementById("featured-button");
 
 
 
-let images=[
-  {
-    firstImage:"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/134/697/original/How_to_Make_Cold_Brew_Coffee_At_Home_in_7_Easy_Steps.jpeg?1721595439",
-    secondImage:"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/134/690/original/Low_Calorie_Homemade_Cold_Brew_Coffee.jpeg?1721594761",
-    thirdImage:"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/134/523/original/The_School_of_Styling.jpeg?1721501583"
-  }
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //latest category
 
@@ -70,6 +47,7 @@ imageThree.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000
 
 
 function featuredButton(){
+  //prices
     priceOne.innerHTML=`$98.00`;
     priceTwo.innerHTML=`$122.00`;
      priceThree.innerHTML=`$123.00`;
@@ -84,11 +62,31 @@ function featuredButton(){
 imageOne.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/135/285/original/Caf%C3%A9_espagnol.jpeg?1721837679";
 imageTwo.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/135/288/original/Australian_Flat_White.jpeg?1721838424";
 imageThree.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/134/671/original/download_%2826%29.jpeg?1721591668";
+ 
+}
 
 
-   
+
+function bestSellerButton(){
+  //Prices
+  priceOne.innerHTML=`$98.00`;
+  priceTwo.innerHTML=`$122.00`;
+   priceThree.innerHTML=`$123.00`;
+ 
+ //names
+ nameOne.innerHTML=`Expresso`;
+ nameTwo.innerHTML=`Latte`;
+ nameThree.innerHTML=`Cappuccino`;
+ 
+//images
+
+imageOne.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/135/296/original/download_%2836%29.jpeg?1721841521";
+imageTwo.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/135/301/original/_50_Pack__Disposable_Strawless_Plastic_Cups_with_Lids_-_24_Oz_Clear_Plastic_Cups_and_Sippy_Cups_Lids__Perfect_Eco-Friendly_To_Go_Cups_for_Iced_Coffee__Smoothies__Soda_Party_Drinks_and_Cafe_Essentials.jpeg?1721841933";
+imageThree.src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/135/308/original/Latte_Macciato.jpeg?1721842506";
+
 }
 
 //buttons on clicks for top category
 lastestButtonOnclick.addEventListener("click", latestButton);
 featuredButtonOnclick.addEventListener("click", featuredButton);
+bestSellerButttonOnclick.addEventListener("click", bestSellerButton);
